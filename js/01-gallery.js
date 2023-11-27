2181b443dba5a251d39a46400683a3ca5091afae
Shershylia <mariaadakova@gmail.com>
2023-11-27 20:11:47 +0200

    ready

---
+++ js/01-gallery.js
@@ -0,0 +1,43 @@
+import { galleryItems } from './gallery-items.js';
+console.log(galleryItems);
+// Change code below this line
+const list = document.querySelector(".gallery");
+list.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+
+
+function createMarkup(arr) {
+    return arr
+        .map(
+            ({ preview, original, description }) =>
+                `<li class="gallery__item">
+                    <a class="gallery__link" href="${original}">
+                        <img
+                            class="gallery__image"
+                            src="${preview}"
+                            data-source="${original}"
+                            alt="${description}"
+                        />
+                    </a>
+                </li>`
+        )
+        .join("");
+}
+
+list.addEventListener("click", handleClick);
+
+function handleClick(event) {
+    event.preventDefault();
+if (event.target === event.currentTarget) {
+    return;
+}
+    const liClick = event.target.closest(".gallery__item");
+    console.log(liClick);
+
+
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+
+instance.show()
+}
+
---
+++ js/02-lightbox.js
@@ -0,0 +1,35 @@
+import { galleryItems } from "./gallery-items.js";
+
+// Change code below this line
+
+console.log(galleryItems);
+const list = document.querySelector(".gallery");
+list.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+    )
+    .join("");
+}
+
+list.addEventListener("click", handleClick);
+
+function handleClick(event) {
+  event.preventDefault();
+  if (event.target === event.currentTarget) {
+    return;
+  }
+
+  const instance = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+    captionPosition: "bottom",
+  });
+}
